73780a28a219edc9da583874b71b484427b40131
aGupta97uk <agupta97uk@gmail.com>
2019-01-21 03:27:04 +0000

    GUI Dev

--- Interface.py
+++ Interface.py
@@ -1,36 +1,61 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import ttk
 
 
 class Window(tk.Tk):
 
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self,  *args, **kwargs)
+    def __init__(self):
+        # self.tk = Tk()
+
+        tk.Tk.__init__(self)
+        tk.Tk.iconbitmap(self)
+        tk.Tk.wm_title(self)
 
         container = tk.Frame(self)
-        container.pack()
-        container.grid_rowconfigure()
-        container.grid_columnconfigure()
 
-        frame = F(container, self)
-        self.frame[F] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
+        container.grid_rowconfigure(10, weight=1)
+        container.grid_columnconfigure(10, weight=1)
 
-        self.show_frame(HomePage)
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
 
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
 
-class HomePage(tk.Frame):
+class StartPage(tk.Frame):
 
-    def __init__(self, parent):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+
+        # Label
+        lab = tk.Label(self, text="StartPage")
+        lab.grid(row=3, column=5)
+
+        # Button
+        button1 = ttk.Button(self, text="Button1", command=lambda: controller.show_frame(PageOne))
+        button1.grid(row=1, column=1)
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        label = tk.Label(self, text = "Graph", font=("Helvetica", 12))
-        label.pack(pady=10, padx=10)
+        # Label
+        lab = tk.Label(self, text="PageOne")
+        lab.grid(row=3, column=5)
+
+        # Button
+        button1 = ttk.Button(self, text="Button1", command=lambda: controller.show_frame(StartPage))
+        button1.grid(row=1, column=1)
 
 
 root = Window()
