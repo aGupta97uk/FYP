d713d3b2f32f705596503293711b1b0ae4be4db7
aGupta97uk <agupta97uk@gmail.com>
2019-01-22 05:07:26 +0000

    Embedding
    Embedding the figure into the canvas complete. May Require some positioning changes to make it look better.

--- Interface.py
+++ Interface.py
@@ -1,61 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
 
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import *
+from matplotlib.figure import Figure
+
 
 class Window(tk.Tk):
 
     def __init__(self):
-        # self.tk = Tk()
 
         tk.Tk.__init__(self)
-        tk.Tk.iconbitmap(self)
-        tk.Tk.wm_title(self)
 
         container = tk.Frame(self)
 
         container.grid_rowconfigure(10, weight=1)
         container.grid_columnconfigure(10, weight=1)
 
-        self.frames = {}
-
-        for F in (StartPage, PageOne):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-
         # Label
-        lab = tk.Label(self, text="StartPage")
-        lab.grid(row=3, column=5)
+        lab = tk.Label(self, text="Label")
+        lab.grid(row=5, column=5)
 
         # Button
-        button1 = ttk.Button(self, text="Button1", command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text="Button 1")
         button1.grid(row=1, column=1)
 
+        # Figure
+        fig = Figure(figsize=(5, 5), dpi=100)
+        a = fig.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 6])
 
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        # Label
-        lab = tk.Label(self, text="PageOne")
-        lab.grid(row=3, column=5)
-
-        # Button
-        button1 = ttk.Button(self, text="Button1", command=lambda: controller.show_frame(StartPage))
-        button1.grid(row=1, column=1)
+        # Embedding the figure into the canvas
+        canvas = FigureCanvasTkAgg(fig, self)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=0)
 
 
 root = Window()
