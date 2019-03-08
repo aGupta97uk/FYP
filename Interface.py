0604993d72bbdc0e0a0b2354ac725dd06d3beeda
aGupta97uk <agupta97uk@gmail.com>
2019-03-08 18:50:45 +0000

    Graph Clearing Sorted

--- Interface.py
+++ Interface.py
@@ -2,11 +2,9 @@ import networkx as nx
 import tkinter as tk
 from tkinter import ttk, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("TkAgg")
-
-import matplotlib.pyplot as plt
 
 root = tk.Tk()
 
@@ -20,39 +18,20 @@ class Window:
         self.main_container.pack(side="top", fill="both", expand=True)
 
         # Left Frame
-        self.left_frame = tk.Frame(self.main_container)
+        self.left_frame = tk.Frame(self.main_container, width=800, relief="ridge", bd=20, borderwidth=3,
+                                         padx=5, pady=5, background='white')
         self.left_frame.pack(side="left", fill="both", expand=True)
 
-        # Left Frame - Top
-        self.left_frame_top = tk.Frame(self.left_frame, relief="ridge", bd=20, borderwidth=3, pady=2, padx=2)
-        self.left_frame_top.pack(side="top", fill="both", expand=True)
-        self.left_frame_top.update_idletasks()
-
-        # Left Frame - Bottom
-        self.left_frame_bottom = tk.Frame(self.left_frame)
-        self.left_frame_bottom.pack(side="bottom", fill="both", expand=True)
-
-        # Text area to print out the results of the algorithm run
-        # FIXME: This makes the top and bottom frames resize, making the top frame smaller
-        # This happens only when Text is used. Label is fine
-        self.left_frame_bottom2_text = tk.Text(self.left_frame_bottom, background="Light Grey", relief="ridge",
-                                               bd=20, borderwidth=3, pady=2, padx=2)
-        self.left_frame_bottom2_text.pack(side="bottom", fill='both', expand=True, anchor="center")
-
         # Right Frame
-        self.right_frame = tk.Frame(self.main_container)
+        self.right_frame = tk.Frame(self.main_container, background='blue')
         self.right_frame.pack(side="right", fill="both", expand=True)
 
-        # Right Frame2
-        self.right_frame2 = tk.Frame(self.right_frame)
-        self.right_frame2.pack(side="right", fill="both", expand=True)
-
         # Right Frame2 - Top
-        self.right_frame2_top = tk.Frame(self.right_frame2, relief="ridge", bd=20, borderwidth=3,
-                                         padx=5, pady=5)
-        self.right_frame2_top.grid(row=0, column=0, sticky="nsew")
-        self.right_frame2.grid_rowconfigure(4, weight=1)
-        self.right_frame2.grid_columnconfigure(0, weight=1)
+        self.right_frame2_top = tk.Frame(self.right_frame, relief="ridge", bd=20, borderwidth=3,
+                                         padx=5, pady=5, background='green')
+        self.right_frame2_top.grid(row=0, column=1, sticky="nsew")
+        self.right_frame.grid_rowconfigure(0, weight=1)
+        self.right_frame.grid_columnconfigure(1, weight=1)
 
         # Label - Generate Random Graph
         # FIXME: The font size - not changing
@@ -91,14 +70,49 @@ class Window:
         self.button_grg.grid(row=3, column=1)
         self.button_grg.grid_rowconfigure(1, weight=1)
 
+        # Creating the Figure
+        fig_num = 1
+        self.figure = plt.figure(num=fig_num, dpi=100, clear=True)
+        self.ax = self.figure.add_subplot(111)
+
+        # Defining the canvas
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.left_frame)
+
+    #
+    #     # Right Frame2 - Middle
+    #     self.right_frame2_mid = tk.Frame(self.right_frame2, relief="ridge", bd=20, borderwidth=3,
+    #                                      padx=5, pady=5)
+    #     self.right_frame2_mid.grid(row=4, column=0, sticky="nsew")
+    #     self.right_frame2.grid_rowconfigure(4, weight=1)
+    #     self.right_frame2.grid_columnconfigure(0, weight=1)
+    #
+    #     # Label - Algorithms
+    #     self.lab_alg = tk.Label(self.right_frame2_mid, text="Algorithms",
+    #                             font="24")
+    #     self.lab_alg.grid(row=0, column=0, padx=10, pady=10, in_=self.right_frame2_mid)
+    #
+    #     # Right Frame2 - Bottom
+    #     self.right_frame2_bottom = tk.Frame(self.right_frame2, relief="ridge", bd=20, borderwidth=3,
+    #                                      padx=5, pady=5)
+    #     self.right_frame2_bottom.grid(row=8, column=0, sticky="nsew")
+    #     self.right_frame2.grid_rowconfigure(4, weight=1)
+    #     self.right_frame2.grid_columnconfigure(0, weight=1)
+    #
+    #     # # Text area to print out the results of the algorithm run
+    #     # # FIXME: This makes the top and bottom frames resize, making the top frame smaller
+    #     # # This happens only when Text is used. Label is fine
+    #     # self.right_frame2_bottom_text = tk.Text(self.right_frame2_bottom, background="Light Grey", relief="ridge",
+    #     #                                        bd=20, borderwidth=3, pady=2, padx=2)
+    #     # self.right_frame2_bottom_text.grid(side="bottom", fill='both', expand=True, anchor="center")
+    #
     # Getting the number of vertices for the random graph
     # Performs validation checks on the user input to make sure the input entered is correct
     # TODO: Add a upper limit to the input validation
     # FIXME: A new graph is drawn every time underneath the old one
+
     def get_graph(self):
 
-        string_vertices \
-            = self.vertices_num.get()
+        string_vertices = self.vertices_num.get()
         string_edges = self.edges_num.get()
 
         try:
@@ -142,25 +156,29 @@ class Window:
     # Drawing graph in the top left frame
     def draw_graph(self, vertices, edges):
 
-        # Embedding the figure
-        f = Figure(figsize=(1, 1), dpi=100)
-        a = f.add_subplot(111)
-
-        # TODO: Show the labels on the vertices in the graph
+        self.ax.clear()
 
         # TODO: Change the type of graph so only a complete graph is generated
+        # NetworkX Graph
         graph = nx.dense_gnm_random_graph(vertices, edges)
-        pos = nx.random_layout(graph)
-        nx.draw(graph, pos, ax=a, with_labels=True)
+        pos = nx.spring_layout(graph)
+        # Draw Nodes
+        nx.draw_networkx_nodes(graph, pos, ax=self.ax, node_size=700)
+        # Draw Edges
+        nx.draw_networkx_edges(graph, pos, width=2)
+        # Draw labels
+        nx.draw_networkx_labels(graph, pos, font_size=10, font_family='sans-serif')
+        # Turn off the axis
+        plt.axis('off')
 
-        canvas = FigureCanvasTkAgg(f, master=self.left_frame_top)
-        canvas.draw()
-        canvas.get_tk_widget().pack(fill="both", expand=True)
+        # Drawing the figure using the renderer
+        self.canvas.draw()
+        # Positioning the canvas using pack
+        self.canvas.get_tk_widget().pack(side="left", anchor="center", fill="both", expand=True)
 
         print("Number of edges in graph: ", nx.info(graph))
 
         num_vertices = nx.number_of_nodes(graph)
-
         num_edges = nx.number_of_edges(graph)
 
         print("Number of Vertices = ", num_vertices)
@@ -171,7 +189,7 @@ class Window:
 root.title("Graph")
 # Setting the window size based on the screen size
 root.pack_propagate(0)
-# Making the window fullscreen
+# Making the window ful-screen
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 print(screen_width, " ", screen_height)
--- Main.py
+++ Main.py
@@ -3,39 +3,39 @@ import matplotlib.pyplot as plt
 
 from Interface import Window
 
-G = nx.Graph()
-
-# Vertex
-G.add_node('A')
-G.add_node('B')
-G.add_node('C')
-G.add_node('D')
-G.add_node('E')
-G.add_node('F')
-
-# Edge
-G.add_edge('A', 'B', weight=1)
-G.add_edge('A', 'C', weight=2)
-G.add_edge('A', 'D', weight=3)
-G.add_edge('A', 'E', weight=4)
-G.add_edge('A', 'F', weight=5)
-G.add_edge('B', 'C', weight=6)
-
-# Using the shortest path algorithm
-print(nx.shortest_path(G, 'C', 'B', weight='weight'))
-
-# Some Information about the graph
-print(nx.info(G))
-
-print("Number of edges:", nx.number_of_edges(G))
-
-print(G.node)
-
-# draw graph
-nx.draw_networkx(G, pos=nx.spring_layout(G, weight='weight'))
-# turn off the axis on the graph
-plt.axis('off')
-# display the drawn graph
-plt.show()
+# G = nx.Graph()
+#
+# # Vertex
+# G.add_node('A')
+# G.add_node('B')
+# G.add_node('C')
+# G.add_node('D')
+# G.add_node('E')
+# G.add_node('F')
+#
+# # Edge
+# G.add_edge('A', 'B', weight=1)
+# G.add_edge('A', 'C', weight=2)
+# G.add_edge('A', 'D', weight=3)
+# G.add_edge('A', 'E', weight=4)
+# G.add_edge('A', 'F', weight=5)
+# G.add_edge('B', 'C', weight=6)
+#
+# # Using the shortest path algorithm
+# print(nx.shortest_path(G, 'C', 'B', weight='weight'))
+#
+# # Some Information about the graph
+# print(nx.info(G))
+#
+# print("Number of edges:", nx.number_of_edges(G))
+#
+# print(G.node)
+#
+# # draw graph
+# nx.draw_networkx(G, pos=nx.spring_layout(G, weight='weight'))
+# # turn off the axis on the graph
+# plt.axis('off')
+# # display the drawn graph
+# plt.show()
 
 Window()
